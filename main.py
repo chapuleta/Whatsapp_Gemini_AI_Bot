f6d919ad593f199ba9f1db3961af878bf5ab2f49
chapuleta <130189521+chapuleta@users.noreply.github.com>
2025-06-16 00:07:29 -0300

    só pra forçar o vercel

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask,request,jsonify
 import requests
 import os
 import fitz
-
+#forcar vercel a mudar
 wa_token=os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
 phone_id=os.environ.get("PHONE_ID")
